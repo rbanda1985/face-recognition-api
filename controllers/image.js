84fc9321736e3c644e9192eca3b4d732f80968d1
rbanda1985 <robertbanda85@gmail.com>
2023-03-06 15:36:50 -0600

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '2930ba8fd4144ec182265dc6ca72fb65'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(
+    {
+      id: 'face-detection',
+      name: 'face-detection',
+      version: '6dc7e46bc9124c5c8824be4822abe105',
+      type: 'visual-detector',
+    }, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries)
+  })
+  .catch(err => response.status(400).json('Unable to get entries'));
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
