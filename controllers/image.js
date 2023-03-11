eaf00ddd8161f0b4cc22414774e7532736b91ee5
rbanda1985 <robertbanda85@gmail.com>
2023-03-11 16:27:15 -0600

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: process.env.API_CLARIFAI
+  apiKey: process.env.API_ClARIFAI
 });
 
 const handleApiCall = (req, res) => {
