2933d06f8f15f24117177a20ffe64dc21f3bda79
rbanda1985 <robertbanda85@gmail.com>
2023-03-15 06:56:19 -0500

    Adding database connection

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DB_HOST,
-    port : 5432,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    host : process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
